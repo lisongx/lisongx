b16675dff8eab73ce63ea0edbeafe13300a9d28f
Li Song <lisong@notimportant.org>
2020-07-12 01:24:51 +0100

    test a bit of content

--- update_readme.py
+++ update_readme.py
@@ -33,5 +33,9 @@ def upload_image():
 if __name__ == "__main__":
     readme = root / "README.md"
     image_url = upload_image()
-    content = "# I'm sitting on my Github profile\n![I'm sitting on my Github profile](%s)" % image_url
+    content = """
+        #I'm sitting on my Github profile
+        <img src="https://66.media.tumblr.com/9928d0e510741aed0863efeca4fce19b/tumblr_ndsqgsv7331tk1vn4o1_400.gifv" width="100%" />
+        ![I'm sitting on my Github profile](%s)"
+    """ % image_url
     readme.open("w").write(content)
